1d18e038c96ae0df952dfcb3a1d448cbba6767bc
nirnanaaa <mosny@zyg.li>
2014-01-30 13:04:04 +0100

    added runtime dart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def dart_include_tag
     content_tag(:script, nil, :src => asset_path("main.dart"), :type => 'application/dart') +
-    content_tag(:script, nil, src: asset_path("dart.js"), type: 'text/javascript')
+    content_tag(:script, nil, src: asset_path("runtime.dart.js"), type: 'text/javascript')
   end
 end
