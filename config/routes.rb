9621b898bfcd428d19131b239df33a4a8a141d86
nirnanaaa <mosny@zyg.li>
2014-01-30 15:07:19 +0100

    added robots

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Homepage::Application.routes.draw do
   resources :pages
   get 'schedules' => 'schedules#index', as: :index_schedules
+  
+  get 'robots.txt' => 'robots#index'
   root "pages#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
