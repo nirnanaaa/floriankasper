82a0c88a7ec0978555ea58e51b3ed6c018cf5f12
nirnanaaa <mosny@zyg.li>
2014-02-21 11:08:02 +0100

    changed asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Homepage::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = "//s3.amazonaws.com/#{ENV['FOG_DIRECTORY']}"
+  config.action_controller.asset_host = "//s3-eu-west-1.amazonaws.com/senter"
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
