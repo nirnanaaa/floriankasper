7155d2f8971107526aa3d52f63e4e3434d5a988f
nirnanaaa <mosny@zyg.li>
2014-08-07 11:22:21 +0200

    updated airbrake on errors controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,11 @@
 class ErrorsController < ApplicationController
   def show
     @exception = env["action_dispatch.exception"]
+    Airbrake.notify_or_ignore(
+      @exception,
+      :parameters => params,
+      :cgi_data => ENV.to_hash
+    )
     respond_to do |format|
       format.html{ render action: params[:status]}
     end
