a899fe2bccbe7d38810b00d40e4af59e1a807196
nirnanaaa <mosny@zyg.li>
2014-08-07 11:29:22 +0200

    rendering only 404s not

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,13 +1,8 @@
 class ErrorsController < ApplicationController
   def show
     @exception = env["action_dispatch.exception"]
-    Airbrake.notify_or_ignore(
-      @exception,
-      :parameters => params,
-      :cgi_data => ENV.to_hash
-    )
     respond_to do |format|
-      format.html{ render action: params[:status]}
+      format.html{ render '404'}
     end
   end
 
